fb9fd0dc392e4f7f8e7fe8ede085ea4d3f160e0f
JoanValença <Joan_fsacat@outlook.com>
2023-09-18 19:15:36 -0300

    feat: Adcionar funcionalidade JavaScript

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+const modalOpacity = document.querySelector(".modalOpacity");
+const popUpCloseButton = document.querySelector(".popUp__close_button");
+const profileButtonedit = document.querySelector(".profile__button-edit");
+const popupSubmit = document.querySelector(".popUp__submit");
+const popupMe = document.querySelector("#me");
+const popupAboutMe = document.querySelector("#aboutme");
+const profileName = document.querySelector(".profile__Name");
+const profileAboutMe = document.querySelector(".profile__aboutMe");
+const popup = document.querySelector(".popUp");
+
+function popupToggle(e) {
+  modalOpacity.classList.toggle("modalOpacityOpen");
+  popupMe.value = profileName.textContent;
+  popupAboutMe.value = profileAboutMe.textContent;
+  popupSubmit.disabled = true;
+}
+
+function changeInput() {
+  const isTrue = popupMe.value === "" || popupAboutMe.value === "";
+  popupSubmit.disabled = isTrue;
+}
+
+function popupEditNameAndAboutMe(e) {
+  e.preventDefault();
+  profileName.textContent = popupMe.value;
+  profileAboutMe.textContent = popupAboutMe.value;
+  popupToggle();
+}
+
+profileButtonedit.addEventListener("click", popupToggle);
+popUpCloseButton.addEventListener("click", popupToggle);
+
+popupSubmit.addEventListener("click", popupEditNameAndAboutMe);
+popup.addEventListener("input", changeInput);
+
+// profileButtonedit.addEventListener("click", function () {
+//   modalOpacity.style.display = "flex";
+//   console.log("funciona");
+// });
+
+// popUpCloseButton.addEventListener("click", function () {
+//   modalOpacity.style.display = "none";
+// });
